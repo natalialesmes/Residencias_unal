2f57cf1adc17003adc0e31faf65944fb05c4a2d9
Natalia Lesmes <94539622+natalialesmes@users.noreply.github.com>
2025-07-15 08:45:54 -0500

    implementación AlmacenamientoMemoria.java

--- src/main/java/residencias/datos/AlmacenamientoMemoria.java
+++ src/main/java/residencias/datos/AlmacenamientoMemoria.java
@@ -1,4 +1,38 @@
 package residencias.datos;
 //Almacenar los datos en RAM
 public class AlmacenamientoMemoria {
+    private List<Estudiante> estudiantes;
+
+    public AlmacenamientoMemoria() {
+        estudiantes = new ArrayList<>();
+    }
+
+    public boolean agregarEstudiante(Estudiante e) {
+        if (buscarPorId(e.getId()) == null) {
+            estudiantes.add(e);
+            return true;
+        }
+        return false; // Ya existe un estudiante con ese ID
+    }
+
+    public boolean eliminarEstudiantePorId(String id) {
+        return estudiantes.removeIf(e -> e.getId().equals(id));
+    }
+
+    public List<Estudiante> obtenerTodos() {
+        return new ArrayList<>(estudiantes);
+    }
+
+    public Estudiante buscarPorId(String id) {
+        for (Estudiante e : estudiantes) {
+            if (e.getId().equals(id)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public int cantidad() {
+        return estudiantes.size();
+    }
 }
